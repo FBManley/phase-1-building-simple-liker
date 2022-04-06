972cfb7bd5a82af980f3b6d5853586f8162a5420
Jacob Manley <jbmanley45@gmail.com>
2022-04-06 12:14:47 -0700

    commit tree 7a10633241fa713f4e7d39a7358c400c299d3daf

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.getElementById("modal").className = "hidden"
 
 
-
+//addEventListener()
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -23,3 +24,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+//You should only need to make one change to the HTML, and no changes to the CSS file.
+//Your goal is to implement the "liking" functionality of "Simple Liker."
+// updating the appearance of the heart if it returns a "successful" response, and displaying an error in the DOM otherwise
